72c31cf4c3fc57531d9008115ad2d6442d79bc94
Shreeram Jadhav <shreeramjadhav26@gmail.com>
2025-06-27 14:35:37 +0530

    Add the logic of status subcommand

--- myGit/src/status.py
+++ myGit/src/status.py
@@ -1,3 +1,19 @@
-def execute(files):
-    for file in files:
-        print(f"Adding {file} to the staging area...")
+import os
+
+def execute(args):
+    index_path = ".mygit/index"
+
+    if not os.path.exists(index_path):
+        print("Error: No Repository initialized. Please run 'mygit init' first.")
+        return
+    
+    if os.path.getsize(index_path) == 0:
+        print("No files staged for commit.")
+        return
+    
+    with open(index_path, "r") as f:
+        staged_files = f.read() 
+    
+    print("Staged files:")
+    for line in staged_files.splitlines():
+        print(" -", line)
